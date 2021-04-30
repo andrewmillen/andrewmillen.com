da05a511d15b392c56ada6fa38c96581ac6f21a9
andrewmillen <andrewmillen1@gmail.com>
2021-04-29 21:20:47 -0500

    Add preview functionality

--- pages/_document.js
+++ pages/_document.js
@@ -3,22 +3,22 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    const { pathname } = ctx;
     return { ...initialProps };
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
+        <Head>
           <script
             async
             defer
-            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=andrewmillen"
+            src={`https://static.cdn.prismic.io/prismic.js?new=true&repo=${process.env.PRISMIC_REPOSITORY_NAME}`}
           ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
