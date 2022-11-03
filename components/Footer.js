5f0830448ae45748e5f00b2fa3537c31959a0ba4
andrewmillen <andrewmillen1@gmail.com>
2022-11-03 17:30:01 -0500

    Forgot to save files on last commit, I'm great at this

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer({ text }) {
       <div className="container">
         <svg viewBox="0 0 172 172" className="w-10 h-10 mb-8 mx-auto">
           <path
-            class="st0"
+            className="st0"
             d="M86,0C38.5,0,0,38.5,0,86s38.5,86,86,86c47.5,0,86-38.5,86-86S133.5,0,86,0z M53.7,126L37.1,79l8.5-33l28.1,80
   H53.7z M89.7,126L61.6,46h20l16.5,47L89.7,126z M128.9,126h-18.6V46h18.6V126z"
           />
