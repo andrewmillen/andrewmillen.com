1df2356eb9a7ae475d83eaf21d64228d7d6e15ea
andrewmillen <andrewmillen1@gmail.com>
2022-11-03 17:26:30 -0500

    Mid-upgrade commit for safety

--- next.config.js
+++ next.config.js
@@ -15,13 +15,13 @@ const nextConfig = {
   },
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(__dirname);
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ["@svgr/webpack"],
-    });
+    // config.module.rules.push({
+    //   test: /\.svg$/,
+    //   issuer: {
+    //     test: /\.(js|ts)x?$/,
+    //   },
+    //   use: ["@svgr/webpack"],
+    // });
     return config;
   },
 };
