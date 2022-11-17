17dec7d9d06434ef746ecf85f1a73030d04e42be
andrewmillen <andrewmillen1@gmail.com>
2022-11-16 19:27:44 -0600

    Clean up next config

--- next.config.js
+++ next.config.js
@@ -10,18 +10,8 @@ const nextConfig = {
       },
     ];
   },
-  // images: {
-  //   domains: ["images.prismic.io"],
-  // },
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(__dirname);
-    // config.module.rules.push({
-    //   test: /\.svg$/,
-    //   issuer: {
-    //     test: /\.(js|ts)x?$/,
-    //   },
-    //   use: ["@svgr/webpack"],
-    // });
     return config;
   },
 };
