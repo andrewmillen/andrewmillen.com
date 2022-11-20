e69b611ea7154f6c008103cc578f1b4dab925762
andrewmillen <andrewmillen1@gmail.com>
2022-11-19 20:35:32 -0600

    Add Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
