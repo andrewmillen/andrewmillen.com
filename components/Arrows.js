a4820b12542a86bb9200463d9662d1849ad5a2af
andrewmillen <andrewmillen1@gmail.com>
2023-10-24 21:43:45 -0500

    Revise arrows in slider

--- components/Arrows.js
+++ components/Arrows.js
@@ -4,9 +4,9 @@ export function PrevArrow({ onClick }) {
       // Not sure where onClick is defined... maybe in Slick?
       // But this doesn't work without it.
       onClick={onClick}
-      className="-left-4 lg:-left-8 xl:-left-8 2xl:-left-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full hover:bg-neutral-100  dark:bg-neutral-900 dark:hover:bg-neutral-800 active:scale-95"
+      className="-left-4 lg:-left-8 xl:-left-8 2xl:-left-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full dark:bg-neutral-900 active:scale-95 text-neutral-500 dark:text-neutral-300 hover:text-neutral-700 dark:hover:text-white transition duration-75"
     >
-      <span className="font-degular font-bold text-neutral-500 dark:text-neutral-300 text-4xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+      <span className="font-degular font-bold text-4xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
         <span className="relative -top-1">←</span>
       </span>
       <span className="sr-only"> Previous</span>
@@ -18,10 +18,10 @@ export function NextArrow({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="-right-4 lg:-right-8 xl:-right-8 2xl:-right-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full hover:bg-neutral-100  dark:bg-neutral-900 dark:hover:bg-neutral-800 active:scale-95"
+      className="-right-4 lg:-right-8 xl:-right-8 2xl:-right-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full dark:bg-neutral-900 active:scale-95 text-neutral-500 dark:text-neutral-300 hover:text-neutral-700 dark:hover:text-white transition duration-75"
     >
       <span className="sr-only">Next </span>
-      <span className="font-degular font-bold text-neutral-500 dark:text-neutral-300 text-4xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+      <span className="font-degular font-bold text-4xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
         <span className="relative -top-1">→</span>
       </span>
     </button>
