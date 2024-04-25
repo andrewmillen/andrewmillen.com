c241fb150e62899f263de651befc7e4160312ad5
andrewmillen <andrewmillen1@gmail.com>
2024-04-25 15:30:50 -0500

    Adjust text style on homepage

--- components/HomepageHeader.js
+++ components/HomepageHeader.js
@@ -11,7 +11,7 @@ export default function HomepageHeader({ content }) {
         <div className="text-3xl lg:text-4xl leading-relaxed mb-4 mt-20 lg:mt-30 xl:mt-40">
           {/* 👋 */}
         </div>
-        <p className="text-xl 2xl:text-2xl leading-relaxed lg:leading-relaxed mb-6 lg:mb-12 text-neutral-800 dark:text-neutral-200 max-w-3xl">
+        <p className="text-xl leading-relaxed lg:leading-relaxed mb-6 lg:mb-12 text-neutral-800 dark:text-neutral-200 max-w-3xl">
           {content.intro}
         </p>
         <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-8 mb-12">
