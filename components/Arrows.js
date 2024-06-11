f5ef851e3ea651226cc6eedac3410a088e988d5c
andrewmillen <andrewmillen1@gmail.com>
2024-06-11 11:10:25 -0500

    Fix slider arrows touching the viewport edges

--- components/Arrows.js
+++ components/Arrows.js
@@ -4,7 +4,7 @@ export function PrevArrow({ onClick }) {
       // Not sure where onClick is defined... maybe in Slick?
       // But this doesn't work without it.
       onClick={onClick}
-      className="-left-4 lg:-left-8 xl:-left-8 2xl:-left-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full dark:bg-neutral-900 active:scale-95 text-neutral-500 dark:text-neutral-300 hover:text-neutral-700 dark:hover:text-white transition duration-75"
+      className="-left-4 lg:-left-4 xl:-left-4 2xl:-left-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full dark:bg-neutral-900 active:scale-95 text-neutral-500 dark:text-neutral-300 hover:text-neutral-700 dark:hover:text-white transition duration-75"
     >
       <span className="font-degular font-bold text-4xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
         <span className="relative -top-1">←</span>
@@ -18,7 +18,7 @@ export function NextArrow({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="-right-4 lg:-right-8 xl:-right-8 2xl:-right-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full dark:bg-neutral-900 active:scale-95 text-neutral-500 dark:text-neutral-300 hover:text-neutral-700 dark:hover:text-white transition duration-75"
+      className="-right-4 lg:-right-4 xl:-right-4 2xl:-right-32 hidden lg:block absolute top-1/2 transform -translate-y-1/2 h-14 w-14 z-10 lg:h-20 lg:w-20 bg-white shadow-lg rounded-full dark:bg-neutral-900 active:scale-95 text-neutral-500 dark:text-neutral-300 hover:text-neutral-700 dark:hover:text-white transition duration-75"
     >
       <span className="sr-only">Next </span>
       <span className="font-degular font-bold text-4xl lg:text-6xl absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
