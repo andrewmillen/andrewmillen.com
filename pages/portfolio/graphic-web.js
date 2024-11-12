5b30ff270a8f63b53452722b958348966a135419
andrewmillen <andrewmillen1@gmail.com>
2024-11-11 21:52:44 -0600

    Portfolio artwork updates

--- pages/portfolio/graphic-web.js
+++ pages/portfolio/graphic-web.js
@@ -9,48 +9,11 @@ export default function GraphicsWeb() {
         <div className="container pb-12 xl:px-0 prose flex flex-col lg:flex-row space-x-8">
           <div className="lg:w-1/2 flex flex-col items-center">
             <ImageWithCaption
-              src="/portfolio-assets/gw-tryptic.jpg"
-              alt="Memphis themed poster series"
+              src="/portfolio-assets/gw-aiga.jpg"
+              alt="A Memphis themed poster featuring the pyramid on the river"
               width="600"
               height="450"
-              caption="Memphis themed poster series"
-            />
-            <ImageWithCaption
-              src="/portfolio-assets/gw-moneyball.jpg"
-              alt="A poster for the movie Moneyball"
-              width="600"
-              height="450"
-              caption="A poster for the movie Moneyball"
-            />
-            <ImageWithCaption
-              src="/portfolio-assets/gw-casino.jpg"
-              alt="Typeface specimen"
-              width="600"
-              height="450"
-              caption="Casino Slab typeface (with <a class='textLink' href='https://andrewmillen.github.io/casino-type-specimen/' target='blank'>interactive specimen</a>)"
-            />
-            <ImageWithCaption
-              src="/portfolio-assets/gw-horns.jpg"
-              alt="Logo of two brass instruments interlocking"
-              width="600"
-              height="450"
-              caption="Logomark for the Memphis Horns, for their <a href='https://memphismusichalloffame.com/inductee/memphishorns/' target='_blank' class='textLink'>Memphis Music Hall of Fame inductee page</a>"
-            />
-          </div>
-          <div className="lg:w-1/2 lg:mt-48 flex flex-col items-center">
-            <ImageWithCaption
-              src="/portfolio-assets/gw-mlk.jpg"
-              alt="A website for the March Against Fear 50-year anniversary service"
-              width="600"
-              height="450"
-              caption="A website for the <a class='textLink' href='https://andrewmillen.github.io/acalltocourage/' target='blank'>March Against Fear 50-year anniversary service</a>"
-            />
-            <ImageWithCaption
-              src="/portfolio-assets/gw-gbb.jpg"
-              alt="Logo for a Grizzlies podcast"
-              width="600"
-              height="450"
-              caption="Logo for a Grizzlies podcast"
+              caption="Memphis-themed poster for an AIGA event"
             />
             <ImageWithCaption
               src="/portfolio-assets/gw-kitchen.jpg"
@@ -60,18 +23,55 @@ export default function GraphicsWeb() {
               caption="Logo for a community kitchen"
             />
             <ImageWithCaption
+              src="/portfolio-assets/gw-moneyball.jpg"
+              alt="Alternative poster for the movie 'Moneyball'"
+              width="600"
+              height="450"
+              caption="Alternative poster for the movie 'Moneyball'"
+            />
+            <ImageWithCaption
+              src="/portfolio-assets/gw-casino.jpg"
+              alt="Typeface specimen"
+              width="600"
+              height="450"
+              caption="A custom typeface created for a work contest, including an <a class='textLink' href='https://andrewmillen.github.io/casino-type-specimen/' target='blank'>interactive specimen</a>"
+            />
+          </div>
+          <div className="lg:w-1/2 lg:mt-48 flex flex-col items-center">
+            <ImageWithCaption
+              src="/portfolio-assets/gw-gbb.jpg"
+              alt="Art for an SBNation NBA podcast"
+              width="600"
+              height="450"
+              caption="Art for an SBNation NBA podcast"
+            />
+            <ImageWithCaption
+              src="/portfolio-assets/gw-mlk.jpg"
+              alt="A website for the March Against Fear 50-year anniversary service"
+              width="600"
+              height="450"
+              caption="A website I designed and coded for the <a class='textLink' href='https://andrewmillen.github.io/acalltocourage/' target='blank'>March Against Fear 50-year anniversary service</a>"
+            />
+            <ImageWithCaption
               src="/portfolio-assets/gw-rhodes.jpg"
               alt="College crest"
               width="600"
               height="450"
-              caption="Simplified crest for my alma mater"
+              caption="Crest for my alma mater"
             />
             <ImageWithCaption
               src="/portfolio-assets/gw-wine.jpg"
-              alt="Website for the Wine Institute"
+              alt="Website for the Wine Institute of California"
               width="600"
               height="450"
-              caption="Website for the Wine Institute"
+              caption="Website for the Wine Institute of California"
+            />
+            <ImageWithCaption
+              src="/portfolio-assets/gw-vexel.jpg"
+              alt="An illustration of a Ford Mustang"
+              width="600"
+              height="450"
+              caption="A <a class='textLink' href='https://en.wikipedia.org/wiki/Vexel' target='blank'>vexel</a> illustration of a Ford Mustang"
             />
           </div>
         </div>
