52f695167214d19447e15dce5e02c68cdfba4f46
andrewmillen <andrewmillen1@gmail.com>
2025-07-21 15:35:55 -0500

    Refactor homepage slider

--- components/FeaturedWorkSlider.js
+++ components/FeaturedWorkSlider.js
@@ -1,12 +1,20 @@
-import { useState, useEffect } from "react";
+import { NextArrow, PrevArrow } from "@/components/Arrows";
+
+import Image from "next/image";
 import Link from "next/link";
 import Slider from "react-slick";
-import { PrevArrow, NextArrow } from "@/components/Arrows";
-import Image from "next/image";
+import { useState } from "react";
 
 export default function FeaturedWorkSlider({ slides }) {
+  const slideBgColors = [
+    "bg-purple-100 dark:bg-purple-900", // hlpr
+    "bg-moss-100 dark:bg-moss-900", // gardening
+    "bg-red-100 dark:bg-red-500", // meal planner
+    "bg-sky-100 dark:bg-blue-900", // other
+  ];
+
   // Set default slide background color for first slide
-  const [slideBg, setSlideBg] = useState(slides[0].bgcolor);
+  const [slideBg, setSlideBg] = useState(slideBgColors[0]);
 
   // Slick slider settings
   const settings = {
@@ -18,8 +26,8 @@ export default function FeaturedWorkSlider({ slides }) {
     nextArrow: <NextArrow />,
     dots: false,
     arrows: true,
-    // Update slideBg by matching the index ("next") of the incoming slide to the object in the Slides array in homepageData.json
-    beforeChange: (current, next) => setSlideBg(slides[next].bgcolor),
+    // Update slideBg by matching the index ("next") of the incoming slide to the object in the Slides array
+    beforeChange: (current, next) => setSlideBg(slideBgColors[next]),
     responsive: [
       {
         breakpoint: 1024,
@@ -35,6 +43,7 @@ export default function FeaturedWorkSlider({ slides }) {
     <div
       className={`sliderWrapper border-b border-neutral-200 dark:border-neutral-800 transition-colors transition-duration-1000 ${slideBg} dark:bg-opacity-80`}
     >
+      {" "}
       <div className="container">
         <Slider
           {...settings}
@@ -42,42 +51,40 @@ export default function FeaturedWorkSlider({ slides }) {
         >
           {slides.map((slide) => (
             <div key={slide.key}>
-              {slide.key === "other" ? (
-                <div className="text-xl sm:text-2xl md:text-3xl font-bold max-w-md md:max-w-2xl">
-                  <p className="text-[#0086D1] dark:text-blue-300 mb-4">
-                    Looking for something more in-depth?
-                  </p>{" "}
-                  <p>
-                    UX work doesn’t always make good eye candy.{" "}
-                    <a
-                      href="mailto:andrewmillen1+website@gmail.com"
-                      target="_blank"
-                      className="underline underline-offset-4 hover:text-neutral-600 dark:hover:text-neutral-300"
-                    >
-                      Email me
-                    </a>{" "}
-                    or{" "}
-                    <Link
-                      href="/portfolio"
-                      className="underline underline-offset-4 hover:text-neutral-600 dark:hover:text-neutral-300"
-                    >
-                      visit my portfolio
-                    </Link>{" "}
-                    to learn more about my process.
-                  </p>
-                </div>
-              ) : (
-                <Image
-                  className="mx-auto"
-                  src={slide.src}
-                  priority="true"
-                  alt={slide.alt}
-                  width={1350}
-                  height={932}
-                />
-              )}
+              <Image
+                className="mx-auto"
+                src={slide.src}
+                priority="true"
+                alt={slide.alt}
+                width={1350}
+                height={932}
+              />
             </div>
           ))}
+          <div key="other">
+            <div className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold max-w-md md:max-w-xl lg:max-w-2xl">
+              <p className="text-sky-600 dark:text-blue-300 mb-8">
+                Looking for something more in-depth?
+              </p>{" "}
+              <p>
+                <a
+                  href="mailto:andrewmillen1+website@gmail.com"
+                  target="_blank"
+                  className="textLink text-neutral-900 dark:text-white hover:text-neutral-600 dark:hover:text-neutral-400"
+                >
+                  Email me
+                </a>{" "}
+                or{" "}
+                <Link
+                  href="/portfolio"
+                  className="textLink text-neutral-900 dark:text-white hover:text-neutral-600 dark:hover:text-neutral-400"
+                >
+                  visit my portfolio
+                </Link>{" "}
+                to learn more about my process.
+              </p>
+            </div>
+          </div>
         </Slider>
       </div>
     </div>
