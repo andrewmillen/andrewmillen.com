1ca74a7367a0f94f140315999a4e32699bd2375a
andrewmillen <andrewmillen1@gmail.com>
2025-07-26 09:47:22 -0500

    Redesign tags

--- components/Tag.js
+++ components/Tag.js
@@ -1,6 +1,6 @@
 export default function Tag({ content }) {
   return (
-    <div className="inline-block uppercase bg-yellow-200 dark:bg-yellow-300 font-display font-semibold px-3 py-1 rounded-full text-sm leading-none md:text-base md:leading-none text-neutral-900">
+    <div className="inline-block uppercase bg-yellow-100 dark:bg-yellow-300 px-4 py-1 font-semibold rounded-full text-sm text-yellow-700">
       {content}
     </div>
   );
