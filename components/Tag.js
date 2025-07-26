73a4807469933d823b5f11201b0fae06743de089
andrewmillen <andrewmillen1@gmail.com>
2025-07-26 12:57:59 -0500

    Fix tag color

--- components/Tag.js
+++ components/Tag.js
@@ -1,6 +1,6 @@
 export default function Tag({ content }) {
   return (
-    <div className="inline-block uppercase bg-yellow-200 dark:bg-yellow-500/10 px-4 py-1 font-semibold dark:font-normal rounded-full text-sm text-yellow-700 dark:text-yellow-200">
+    <div className="inline-block uppercase bg-yellow-100 dark:bg-yellow-500/10 px-4 py-1 font-semibold dark:font-normal rounded-full text-sm text-yellow-700 dark:text-yellow-200">
       {content}
     </div>
   );
