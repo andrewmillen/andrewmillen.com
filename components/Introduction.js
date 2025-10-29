4a2597c89dcbb36459063c5d9913d133b8d49ba8
andrewmillen <andrewmillen1@gmail.com>
2025-10-29 16:34:16 -0500

    Redesign homepage lede

--- components/Introduction.js
+++ components/Introduction.js
@@ -1,17 +1,21 @@
+import Breadcrumb from "@/components/Breadcrumb";
+
 export default function Introduction({ content }) {
   return (
     <header>
-      <div className="container py-[8vw] 2xl:py-28">
-        <h1 className="h1">{content.name}</h1>
-        <p className="h1">{content.title}</p>
-
-        <p className="text-base xs:text-lg leading-normal lg:leading-relaxed xl:text-xl mt-20 md:mt-32 lg:mt-40 xl:mt-52 mb-8 lg:mb-12 text-neutral-800 dark:text-neutral-200 max-w-xl lg:max-w-3xl">
-          {content.summary}
-        </p>
+      <div className="container min-h-[90vh] flex flex-col py-12 xl:py-20 gap-24">
+        <Breadcrumb />
+        <div className="flex flex-col justify-center flex-1">
+          <h1 className="h2 font-bold font-display">{content.lead}</h1>
+          <p className="h2 font-bold font-display text-neutral-600 dark:text-neutral-400 mb-0">
+            {` `}
+            {content.summary}
+          </p>
+        </div>
         <ul className="flex flex-col space-y-4 xs:flex-row xs:space-y-0 xs:space-x-8 mb-4">
           {content.quickLinks.map((link, index) => (
             <li key={index}>
-              <span className="inline-block mr-2 text-xl">{link.emoji}</span>
+              {link.emoji && <span className="text-xl mr-2">{link.emoji}</span>}
               <a
                 href={link.url}
                 className="textLink font-display font-semibold text-xl"
